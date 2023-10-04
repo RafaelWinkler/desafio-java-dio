f2f789708bf494e9e5182e2977c15d03101f4376
RafaelWinkler <rafaeldiaswinkler41@gmail.com>
2023-10-04 10:20:21 -0300

    finished project

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("====================================================");
+        System.out.println("Digitar dados da nova conta: ");
+        System.out.print("Número da agência: ");
+        int agency = sc.nextInt();
+        System.out.print("Número: ");
+        String accountNumber =   sc.next();
+        System.out.print("Nome do Cliente: ");
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.print("Saldo: ");
+        Double balance  = sc.nextDouble();
+        System.out.println("====================================================");
+        System.out.println(name
+                + ", obrigado por criar uma conta em nosso banco,\nsua agência é "
+                + agency
+                +  ", conta "
+                + accountNumber
+                + " e seu saldo é "
+                + String.format("%.2f", balance) + "\njá está disponível para saque.");
+
+        System.out.println("\n====================================================");
+
+        sc.close();
     }
 }
